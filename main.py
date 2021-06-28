6d63a87d0103e3debf75822bb3077b69d42da080
Weaselcore <42705062+Weaselcore@users.noreply.github.com>
2021-06-28 12:44:34 +1200

    Added  multi-line docstrings.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-# This bot was developed by Chris Pham and Wesley Heath (TM).
-# This bot was created for the Discord server UoA Esports.
-# All enquiries please contact chrisphamdev@gmail.com
-# All use of this bot must be approved by its authors.
+"""
+    This bot was developed by Chris Pham and Wesley Heath (TM).
+    This bot was created for the Discord server UoA Esports.
+    All enquiries please contact chrisphamdev@gmail.com
+    All use of this bot must be approved by its authors.
+"""
 
 import asyncio
 import discord
@@ -9,6 +11,7 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 
 # Import the implemented functionalities from different modules
+# TODO Create an extension for these commands.
 from general_commands.basiccommands import *
 from general_commands.reaction_listener import *
 
--- run.py
+++ run.py
@@ -1,7 +1,9 @@
-# This bot was developed by Chris Pham and Wesley Heath (TM).
-# This bot was created for the Discord server UoA Esports.
-# All enquiries please contact chrisphamdev@gmail.com
-# All use of this bot must be approved by its authors.
+"""
+    This bot was developed by Chris Pham and Wesley Heath (TM).
+    This bot was created for the Discord server UoA Esports.
+    All enquiries please contact chrisphamdev@gmail.com
+    All use of this bot must be approved by its authors.
+"""
 
 # TO RUN THE BOT, RUN THIS FILE
 import json
