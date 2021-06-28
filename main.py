b975219a0578ab18f0e9ca6e72e67eb82b4b3f1a
Weaselcore <42705062+Weaselcore@users.noreply.github.com>
2021-06-28 12:38:18 +1200

    Formatting clean up

--- main.py
+++ main.py
@@ -3,17 +3,16 @@
 # All enquiries please contact chrisphamdev@gmail.com
 # All use of this bot must be approved by its authors.
 
-
+import asyncio
 import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
-import asyncio
+
+# Import the implemented functionalities from different modules
+from general_commands.basiccommands import *
+from general_commands.reaction_listener import *
 
 intents = discord.Intents.default()
 intents.members = True
 
 bot = commands.Bot(command_prefix=';', intents=intents)
-
-# Import the implemented functionalities from different modules
-from general_commands.basiccommands import *
-from general_commands.reaction_listener import *
