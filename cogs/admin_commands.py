80694630c2479ae60fa226c21f255525cd96893a
Weaselcore <42705062+Weaselcore@users.noreply.github.com>
2021-06-30 15:53:45 +1200

    Config (#12)
    * Initial commit
    
    * Formatting clean up
    
    * More formatting to appease linter.
    
    * Added  multi-line docstrings.
    
    * More reformatting
    
    * Update pylint.yml
    
    * More formatting
    
    * Added ConfigHelper Class
    
    - Autogenerates json file.
    - Adds, updates and removes IDs when generated.
    - Created a JsonHelper static class to reduce code to access/update data via Json.
    - Refactored code into Cogs for easier management.
    - Added a command to view stored IDs.

---
+++ cogs/admin_commands.py
@@ -0,0 +1,209 @@
+"""
+    This file is for all the admin commands to setup and to remove the event from the server.
+"""
+
+import discord
+import asyncio
+
+from discord.ext import commands
+from discord.utils import get
+import json
+
+from helper.EmbedCreator import EmbedCreator
+from discord.colour import Colour
+
+
+class AdminCog(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        # TODO implement custom help command.
+        self.bot.remove_command('help')
+        print('AdminCog has been initialised.')
+
+    @commands.command()
+    async def setup(self, ctx):
+        try:
+            config_helper = self.bot.config_helper
+            # Grab id for the respective guild.
+            guild = ctx.guild
+
+            # Add halloween event category for organisation.
+            event_category = await guild.create_category("Halloween Event", overwrites=None, reason=None)
+            config_helper.category_id = event_category.id
+            # Add info text channel under Halloween Event category.
+            info_channel = await guild.create_text_channel(
+                'Info',
+                overwrites=None,
+                category=event_category,
+                reason=None
+            )
+            config_helper.info_channel_id = info_channel.id
+
+            boss_channel = await guild.create_text_channel(
+                'Boss',
+                overwrites=None,
+                category=event_category,
+                reason=None
+            )
+            config_helper.boss_channel_id = boss_channel.id
+
+            hunting_channel = await guild.create_text_channel(
+                'Hunting Ground',
+                overwrites=None,
+                category=event_category,
+                reason=None
+            )
+            config_helper.hunting_channel_id = hunting_channel.id
+
+            shop_channel = await guild.create_text_channel(
+                'Shop',
+                overwrites=None,
+                category=event_category,
+                reason=None
+            )
+            config_helper.shop_channel_id = shop_channel.id
+
+        except Exception as e:
+            print(e)
+
+    @commands.command()
+    async def teardown(self, ctx):
+        await ctx.channel.send("```Are you sure you wanna delete this event? Type 'y' or 'yes'```")
+
+        def reply_check(message):
+            return message.content in ['y', 'yes'] and message.channel == ctx.channel
+
+        try:
+            await self.bot.wait_for('message', check=reply_check, timeout=5.0)
+        except asyncio.TimeoutError:
+            await ctx.channel.send("```Command to teardown has expired. Try again.```")
+        else:
+            try:
+                info_channel = ctx.guild.get_channel(self.bot.config_helper.info_channel_id)
+                boss_channel = ctx.guild.get_channel(self.bot.config_helper.boss_channel_id)
+                hunting_channel = ctx.guild.get_channel(self.bot.config_helper.hunting_channel_id)
+                shop_channel = ctx.guild.get_channel(self.bot.config_helper.shop_channel_id)
+                category = get(ctx.guild.categories, id=self.bot.config_helper.category_id)
+
+                await info_channel.delete()
+                del self.bot.config_helper.info_channel_id
+                await boss_channel.delete()
+                del self.bot.config_helper.boss_channel_id
+                await hunting_channel.delete()
+                del self.bot.config_helper.hunting_channel_id
+                await shop_channel.delete()
+                del self.bot.config_helper.shop_channel_id
+                await category.delete()
+                del self.bot.config_helper.category_id
+
+            except Exception as e:
+                print(e)
+
+    @commands.command()
+    async def role_setup(self, ctx, channel_id=None):
+
+        role = None
+        try:
+            # Create role and save it to file.
+            role = await ctx.guild.create_role(
+                name='Hunter',
+                # Appears in the audit log.
+                reason='Add Hunter role for the Halloween Event.',
+                colour=Colour.gold()
+            )
+
+            self.bot.config_helper.role_id = role.id
+
+        except discord.HTTPException:
+            await ctx.send('```Connection Issue: Cannot create Hunter role, try again.```')
+
+        if role:
+            # If the target channel is unspecified, it will send the message to the current channel
+            if channel_id is None:
+                channel_id = ctx.channel.id
+            else:
+                channel_id = int(channel_id)
+
+            # Fetch the target channel
+            target_channel = get(ctx.guild.channels, id=channel_id)
+            # Send a message and react to that message
+            message = await target_channel.send('React to this message to get the Hunter role.')
+            # Register the role message id with the config helper
+            self.bot.config_helper.role_message_id = message.id
+            await message.add_reaction('1\N{variation selector-16}\N{combining enclosing keycap}')
+
+    # This command delete the role from the server (when the event ends)
+    @commands.command()
+    async def hunt_the_hunters(self, ctx):
+        # Find the 'Hunter' role
+        role_name = "Hunter"
+        guild = ctx.guild
+        role_to_remove = None
+
+        role_id = self.bot.config_helper.role_id
+
+        if role_id:
+            role_to_remove = get(guild.roles, id=role_id)
+        else:
+            # Finds the role object to be removed from the guild.
+            for role in guild.roles:
+                if role.name == role_name:
+                    role_to_remove = role
+                    break
+
+        try:
+            await role_to_remove.delete()
+            await ctx.send('```The Hunter role has been deleted.```')
+            del self.bot.config_helper.role_id
+            message_to_delete_id = self.bot.config_helper.role_message_id
+            message_to_delete = await ctx.fetch_message(message_to_delete_id)
+            await message_to_delete.delete()
+            del self.bot.config_helper.role_message_id
+        except discord.HTTPException:
+            await ctx.send(f'```Connection issue: Failed to delete {role_name}, try again.```')
+
+    # Give a medal role to winners
+    @commands.command()
+    async def reward_winners(self, ctx):
+
+        winner_role = None
+        guild = ctx.guild
+        try:
+            winner_role = await guild.create_role(name='Halloween Event Winner 2021', colour=Colour.gold())
+        except discord.HTTPException:
+            await ctx.send(f'```Connection issue: failed to create role, try Again.```')
+        # Check to see if winner_role is not None.
+        if winner_role:
+            # Add the role members in the command to the 'winners' list
+            for member in ctx.message.mentions:
+                try:
+                    await member.add_roles(winner_role)
+                except discord.HTTPException:
+                    await ctx.send(f'```Connection issue: Failed to add roles to {member}, try again.```')
+
+    @commands.command()
+    async def print_id(self, ctx):
+        await ctx.send(f'```{self.bot.config_helper}```')
+
+    # # Replace default help command to this one.
+    # @bot.command()
+    # async def help(ctx):
+    #     await ctx.send('The rule-set can be accessed here.')
+    #     # TO BE ADDED: GAME RULE
+
+    # bot info
+    @commands.command()
+    async def info(self, ctx):
+        info_str = 'This bot was created by Wesley Heath and Chris Pham for the UoA Esports server. Any feedback will '\
+                'be appreciated. '
+        embed = EmbedCreator('Halloween Bot', 'Information', info_str, 'Powered by UoA Esports.')
+        await ctx.send(embed=embed.get_embed())
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f'Pong! This message took {round(self.bot.latency * 1000)}ms to respond.')
+
+
+def setup(bot):
+    bot.add_cog(AdminCog(bot))
--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-# This bot was developed by Chris Pham and Wesley Heath (TM).
-# This bot was created for the Discord server UoA Esports.
-# All enquiries please contact chrisphamdev@gmail.com
-# All use of this bot must be approved by its authors.
+"""
+    This bot was developed by Chris Pham and Wesley Heath (TM).
+    This bot was created for the Discord server UoA Esports.
+    All enquiries please contact chrisphamdev@gmail.com
+    All use of this bot must be approved by its authors.
+"""
 
-
-import discord
-from discord.ext.commands import Bot
-from discord.ext import commands
 import asyncio
+# from discord.ext.commands import Bot
+import discord
+from discord.ext import commands
+from config.ConfigHelper import ConfigHelper
 
 intents = discord.Intents.default()
 intents.members = True
 
 bot = commands.Bot(command_prefix=';', intents=intents)
 
-# Import the implemented functionalities from different modules
-from general_commands.basiccommands import *
-from general_commands.reaction_listener import *
+# Initialise ConfigHelper object and attach it to the bot.
+config_helper = ConfigHelper()
+bot.config_helper = config_helper
+
+
+@bot.event
+async def on_ready():
+    # Future extensions will be called here.
+    bot.load_extension('cogs.admin_commands')
+    bot.load_extension('cogs.reaction_listener')
+    print('Halloween Bot has been deployed.')
